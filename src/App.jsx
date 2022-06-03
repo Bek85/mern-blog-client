1b045093c420622108bf2b84158b21971bd998e4
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-06-03 22:42:37 +0500

    initialize the project with vite

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import TopBar from './topBar/topBar';
+
+function App() {
+  return (
+    <div className="App">
+      <TopBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/topBar/topBar.jsx
@@ -0,0 +1,13 @@
+import './topBar.css';
+
+import React from 'react';
+
+export default function TopBar() {
+  return (
+    <div className="topBar">
+      <div className="topLeft">Left</div>
+      <div className="topCenter">Center</div>
+      <div className="topRight">Right</div>
+    </div>
+  );
+}
