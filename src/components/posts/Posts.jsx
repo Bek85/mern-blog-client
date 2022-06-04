d63ae522395ebcef97072e6c2420f8e678f2099c
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-06-04 18:05:24 +0500

    create markup and styling for <Post/> component

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -1,7 +1,16 @@
+import Post from 'react-blog/components/post/Post';
 import classes from './posts.module.scss';
 
 const { posts } = classes;
 
 export default function Posts() {
-  return <div className={posts}>Posts</div>;
+  return (
+    <div className={posts}>
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+      <Post />
+    </div>
+  );
 }
