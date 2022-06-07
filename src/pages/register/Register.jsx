1926eb6c5fd098dbdb62128c22435c91682fd6d3
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-06-07 20:38:08 +0500

    handle registering a new user and after successful registration redirect to login page

--- src/pages/register/Register.jsx
+++ src/pages/register/Register.jsx
@@ -1,18 +1,52 @@
+import axios from 'axios';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import classes from './register.module.scss';
 
 export default function Register() {
+  const [username, setUsername] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(false);
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    try {
+      const response = await axios.post('api/auth/register', {
+        username,
+        email,
+        password,
+      });
+      response.data && window.location.replace('/login');
+    } catch (error) {
+      setError(true);
+    }
+  };
   return (
     <div className={classes.register}>
       <span className={classes.registerTitle}>Register</span>
-      <form className={classes.registerForm}>
+      <form className={classes.registerForm} onSubmit={handleSubmit}>
         <label>Username</label>
-        <input type="text" placeholder="Enter your username" />
+        <input
+          type="text"
+          placeholder="Enter your username"
+          onChange={(e) => setUsername(e.target.value)}
+        />
         <label>Email</label>
-        <input type="email" placeholder="Enter your email" />
+        <input
+          type="email"
+          placeholder="Enter your email"
+          onChange={(e) => setEmail(e.target.value)}
+        />
         <label>Password</label>
-        <input type="password" placeholder="Enter your password" />
-        <button className={classes.registerBtn}>Register</button>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" className={classes.registerBtn}>
+          Register
+        </button>
         <Link to="/login" className={classes.loginBtn}>
           Login
         </Link>
