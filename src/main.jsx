19a9df5eb17f4b11891b6cb56a39764256c01731
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-06-07 23:12:47 +0500

    create a context to handle login process

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { ContextProvider } from './context/Context';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <ContextProvider>
+        <App />
+      </ContextProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -1,16 +1,48 @@
+import axios from 'axios';
+import { useContext, useRef } from 'react';
 import { Link } from 'react-router-dom';
+import { Context } from '../../context/Context';
 import classes from './login.module.scss';
 
 export default function Login() {
+  const userRef = useRef();
+  const passwordRef = useRef();
+  const { user, dispatch, isFetching } = useContext(Context);
+
+  const handleSubmit = async (evt) => {
+    evt.preventDefault();
+    dispatch({ type: 'LOGIN_START' });
+    try {
+      const response = await axios.post('/api/auth/login', {
+        username: userRef.current.value,
+        password: passwordRef.current.value,
+      });
+      dispatch({ type: 'LOGIN_SUCCESS', payload: response.data });
+    } catch (error) {
+      dispatch({ type: 'LOGIN_FAILURE' });
+    }
+  };
+
+  console.log(user);
   return (
     <div className={classes.login}>
       <span className={classes.loginTitle}>Login</span>
-      <form className={classes.loginForm}>
-        <label>Email</label>
-        <input type="email" placeholder="Enter your email" />
+      <form className={classes.loginForm} onSubmit={handleSubmit}>
+        <label>Username</label>
+        <input
+          type="username"
+          placeholder="Enter your username"
+          ref={userRef}
+        />
         <label>Password</label>
-        <input type="password" placeholder="Enter your password" />
-        <button className={classes.loginBtn}>Login</button>
+        <input
+          type="password"
+          placeholder="Enter your password"
+          ref={passwordRef}
+        />
+        <button type="submit" className={classes.loginBtn}>
+          Login
+        </button>
         <Link to="/register" className={classes.registerBtn}>
           Register
         </Link>
