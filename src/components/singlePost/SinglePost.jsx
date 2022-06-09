b54bdbb64ba6e75f2d40ae8efe48e1c2243a1495
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-06-10 00:45:20 +0500

    handle editing post title and description

--- src/components/singlePost/SinglePost.jsx
+++ src/components/singlePost/SinglePost.jsx
@@ -11,22 +11,40 @@ export default function SinglePost() {
   const location = useLocation();
   const path = location.pathname.split('/')[2];
   const PF = 'http://localhost:4000/images/';
+  const [title, setTitle] = useState('');
+  const [description, setDescription] = useState('');
+  const [updateMode, setUpdateMode] = useState(false);
 
   useEffect(() => {
     const fetchSinglePost = async () => {
       const response = await axios.get(`/api/posts/${path}`);
       setPost(response.data);
+      setTitle(response.data.title);
+      setDescription(response.data.description);
     };
 
     fetchSinglePost();
-  }, [path]);
+  }, [path, updateMode]);
 
   const handleDelete = async () => {
     try {
-      await await axios.delete(`/api/posts/${post._id}`, {
+      await axios.delete(`/api/posts/${post._id}`, {
         data: { username: user.username },
       });
       window.location.replace('/');
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const handleEdit = async () => {
+    try {
+      await axios.put(`/api/posts/${post._id}`, {
+        username: user.username,
+        title,
+        description,
+      });
+      setUpdateMode(false);
     } catch (error) {
       console.log(error);
     }
@@ -42,18 +60,33 @@ export default function SinglePost() {
             className={classes.singlePostImage}
           />
         )}
-        <h1 className={classes.singlePostTitle}>
-          {post.title}
-          {post.username === user?.username && (
-            <div className={classes.singlePostEdit}>
-              <FaEdit className={classes.singlePostIcon} />
-              <FaTrashAlt
-                className={classes.singlePostIcon}
-                onClick={handleDelete}
-              />
-            </div>
-          )}
-        </h1>
+        {updateMode ? (
+          <input
+            type="text"
+            alt=""
+            value={title}
+            className={classes.singlePostTitleInput}
+            autoFocus
+            onChange={(e) => setTitle(e.target.value)}
+          />
+        ) : (
+          <h1 className={classes.singlePostTitle}>
+            {post.title}
+            {post.username === user?.username && (
+              <div className={classes.singlePostEdit}>
+                <FaEdit
+                  className={classes.singlePostIcon}
+                  onClick={() => setUpdateMode(true)}
+                />
+                <FaTrashAlt
+                  className={classes.singlePostIcon}
+                  onClick={handleDelete}
+                />
+              </div>
+            )}
+          </h1>
+        )}
+
         <div className={classes.singlePostInfo}>
           <span className={classes.singlePostAuthor}>
             <Link to={`/?user=${post.username}`}>
@@ -64,7 +97,21 @@ export default function SinglePost() {
             {new Date(post.createdAt).toDateString()}
           </span>
         </div>
-        <p className={classes.singlePostDescription}>{post.description}</p>
+        {updateMode ? (
+          <textarea
+            value={description}
+            type="text"
+            className={classes.singlePostDescriptionInput}
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        ) : (
+          <p className={classes.singlePostDescription}>{post.description}</p>
+        )}
+        {updateMode && (
+          <button onClick={handleEdit} className={classes.singlePostButton}>
+            Save Changes
+          </button>
+        )}
       </div>
     </div>
   );
