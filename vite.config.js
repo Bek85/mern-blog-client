9b14b13d2e55c4c1b475680e14d88b837a644771
Bekhzod Akbarov <stx.webdev@gmail.com>
2022-09-13 17:43:06 +0500

    change proxy endpoint

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
 
   server: {
     proxy: {
-      '/api': 'http://localhost:4000',
+      '/api': 'https://mern-blog-api-production.up.railway.app/',
     },
   },
 
